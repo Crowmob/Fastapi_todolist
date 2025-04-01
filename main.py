679b1bf4132bcc3b4b591dd59ea1a3105e96ba06
valera <valerazakolotiuk@gmail.com>
2025-04-01 22:31:43 +0300

    small corrections

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ async def shutdown():
 async def homepage(request: Request):
     tasks = await db.item.find_many()
     tasks = [(task.id, task.item, task.checked) for task in tasks]
-    print(tasks)
     return templates.TemplateResponse("index.html",{
         "request": request,
         "tasks": tasks
@@ -46,18 +45,16 @@ class DeleteRequest(BaseModel):
     task_id: int
     update: bool
 
+# Delete task
 @app.delete("/delete")
 async def delete_task(data: DeleteRequest):
-    print(data.task_id)
     await db.item.delete(where={"id": data.task_id})
     return {"message": "Deleted task successfully"}
+
 # Update checkbox
 @app.put("/checkbox")
 async def update_checkbox(data: DeleteRequest):
     await db.item.update(data={"checked": data.update}, where={"id": data.task_id})
-    tasks = await db.item.find_many()
-    tasks = [(task.id, task.item, task.checked) for task in tasks]
-    print(tasks)
     return {"message": "Updated checkbox"}
 
 if __name__ == "__main__":
